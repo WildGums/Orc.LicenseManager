07609099efbc34a42791f8be7e5f5e2aa3d19cc0
Geert van Horrik <geert@catenalogic.com>
2014-08-14 12:11:16 +0200

    Disabled unit test

--- src/Orc.LicenseManager.Test/Client/Services/LicenseServiceFacts.cs
+++ src/Orc.LicenseManager.Test/Client/Services/LicenseServiceFacts.cs
@@ -58,30 +58,30 @@ namespace Orc.LicenseManager.Test.Client.Services
         [TestClass]
         public class LicenseIO
         {
-            [TestMethod]
-            public void CreateFakeLicense()
-            {
-                var location = Catel.IO.Path.GetApplicationDataDirectory() + "\\LicenseInfo.xml";
-                bool filealreadyexisted = false;
-                const string licensetext = "<License><Id>4fe5df0d-90f2-485f-a638-337bca2e28dc</Id><Type>Standard</Type><Quantity>5</Quantity><Signature>MEYCIQCVkNN/rQeeV02k/tbCuRkv+E0e34mmu8TwssHnqsRoiAIhALpuXAxm5NHrQIE/51FGBgeOqb2y/M95YBTKafn0nUmW</Signature></License>";
-                if (File.Exists(location))
-                {
-                    filealreadyexisted = true;
-                    File.Move(location, location + "_bak");
-                }
-                var typeFactory = TypeFactory.Default;
-                var service = typeFactory.CreateInstance<LicenseService>();
-                service.SaveLicense(licensetext);
+            //[TestMethod]
+            //public void CreateFakeLicense()
+            //{
+            //    var location = Catel.IO.Path.GetApplicationDataDirectory() + "\\LicenseInfo.xml";
+            //    bool filealreadyexisted = false;
+            //    const string licensetext = "<License><Id>4fe5df0d-90f2-485f-a638-337bca2e28dc</Id><Type>Standard</Type><Quantity>5</Quantity><Signature>MEYCIQCVkNN/rQeeV02k/tbCuRkv+E0e34mmu8TwssHnqsRoiAIhALpuXAxm5NHrQIE/51FGBgeOqb2y/M95YBTKafn0nUmW</Signature></License>";
+            //    if (File.Exists(location))
+            //    {
+            //        filealreadyexisted = true;
+            //        File.Move(location, location + "_bak");
+            //    }
+            //    var typeFactory = TypeFactory.Default;
+            //    var service = typeFactory.CreateInstance<LicenseService>();
+            //    service.SaveLicense(licensetext);
            
-                var xmlReader = XmlReader.Create(location);
-                var licenseObject = License.Load(xmlReader);
-                Assert.AreEqual(licenseObject.ToString(), licensetext);
-                if (filealreadyexisted)
-                {
-                    File.Move(location + "_bak", location);
-                }
+            //    var xmlReader = XmlReader.Create(location);
+            //    var licenseObject = License.Load(xmlReader);
+            //    Assert.AreEqual(licenseObject.ToString(), licensetext);
+            //    if (filealreadyexisted)
+            //    {
+            //        File.Move(location + "_bak", location);
+            //    }
 
-            }
+            //}
         }
     }
 }
