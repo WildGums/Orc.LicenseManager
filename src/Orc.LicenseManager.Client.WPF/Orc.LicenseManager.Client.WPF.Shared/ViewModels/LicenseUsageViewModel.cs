891d0322709150e8c5c855ca7aa6493a4137d508
Tanya Pristupova <zyrk@zyrk.net>
2015-04-16 13:30:33 +0100

    ORCOMP-16 Adds content to LicenseUsage blocking dialog, adds styles

--- src/Orc.LicenseManager.Client.WPF/Orc.LicenseManager.Client.WPF.Shared/ViewModels/LicenseUsageViewModel.cs
+++ src/Orc.LicenseManager.Client.WPF/Orc.LicenseManager.Client.WPF.Shared/ViewModels/LicenseUsageViewModel.cs
@@ -7,15 +7,30 @@
 
 namespace Orc.LicenseManager.ViewModels
 {
+    using System;
+    using System.Collections.Generic;
     using Catel.MVVM;
+    using Catel.Reflection;
+    using Models;
 
     public class LicenseUsageViewModel : ViewModelBase
     {
         #region Constructors
         public LicenseUsageViewModel()
         {
-
+            var assembly = AssemblyHelper.GetEntryAssembly();
+            WindowTitle = assembly.Title() + " licence usage" ;
+            CurrentUsers = new List<NetworkLicenseUsage>()
+            {
+                new NetworkLicenseUsage("12", "222", "Jon", "Licence signature", DateTime.Now),
+                new NetworkLicenseUsage("12", "222", "Jane", "Licence signature", DateTime.Now),
+                new NetworkLicenseUsage("12", "222", "Samuel", "Licence signature", DateTime.Now),
+                new NetworkLicenseUsage("12", "222", "Paula", "Licence signature", DateTime.Now)
+            };
         }
         #endregion
+
+        public string WindowTitle { get; set; }
+        public List<NetworkLicenseUsage> CurrentUsers{ get; set; }
     }
 }
