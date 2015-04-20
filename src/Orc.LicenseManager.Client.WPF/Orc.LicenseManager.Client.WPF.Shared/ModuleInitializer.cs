66507a4ece50a2d2aa6e0c76815976fd4f322058
Geert van Horrik <geert@catenalogic.com>
2015-04-20 14:28:33 +0200

    1) Fixed missing resources in WPF client 2) Fixed website 3) Added new license that is not expired

--- src/Orc.LicenseManager.Client.WPF/Orc.LicenseManager.Client.WPF.Shared/ModuleInitializer.cs
+++ src/Orc.LicenseManager.Client.WPF/Orc.LicenseManager.Client.WPF.Shared/ModuleInitializer.cs
@@ -18,6 +18,6 @@ public static class ModuleInitializer
         serviceLocator.RegisterType<ILicenseVisualizerService, DialogLicenseVisualizerService>();
 
         var languageService = serviceLocator.ResolveType<ILanguageService>();
-        languageService.RegisterLanguageSource(new LanguageResourceSource("Orc.LicenseManager.Client.WPF", "Orc.LicenseManager.Properties", "Resources"));
+        languageService.RegisterLanguageSource(new LanguageResourceSource("Orc.LicenseManager.Client.WPF", "Orc.LicenseManager", "Resources"));
     }
 }
