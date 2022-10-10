f6e28a80f2ce34c3299972fcb4279e2f85efc465
Artem <art.saikin@gmail.com>
2022-10-11 01:27:20 +0300

    Update versions and cleanup

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
+[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyFileVersion("5.0.0")]
 [assembly: AssemblyInformationalVersion("4.0.0-alpha.29")]
-[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2019")]
+[assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2022")]
 
