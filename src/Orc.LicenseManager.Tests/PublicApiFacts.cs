022ce0b2a4a7888ec86bb5d297b4fcfedb715200
Dmitry-VF <fomedima@gmail.com>
2022-11-02 00:46:27 +0300

    Migrate to Verify for API approvals

--- src/Orc.LicenseManager.Tests/PublicApiFacts.cs
+++ src/Orc.LicenseManager.Tests/PublicApiFacts.cs
@@ -1,55 +1,37 @@
 ﻿namespace Orc.LicenseManager.Tests
 {
-    using System.IO;
     using System.Reflection;
     using System.Runtime.CompilerServices;
-    using ApprovalTests;
-    using ApprovalTests.Namers;
+    using System.Threading.Tasks;
     using NUnit.Framework;
     using PublicApiGenerator;
+    using VerifyNUnit;
 
     [TestFixture]
     public class PublicApiFacts
     {
         [Test, MethodImpl(MethodImplOptions.NoInlining)]
-        public void Orc_LicenseManager_Client_HasNoBreakingChanges()
+        public async Task Orc_LicenseManager_Client_HasNoBreakingChanges_Async()
         {
             var assembly = typeof(ApplicationIdService).Assembly;
 
-            PublicApiApprover.ApprovePublicApi(assembly);
+            await PublicApiApprover.ApprovePublicApiAsync(assembly);
         }
 
         [Test, MethodImpl(MethodImplOptions.NoInlining)]
-        public void Orc_LicenseManager_Client_Wpf_HasNoBreakingChanges()
+        public async Task Orc_LicenseManager_Client_Wpf_HasNoBreakingChanges_Async()
         {
             var assembly = typeof(DialogLicenseVisualizerService).Assembly;
 
-            PublicApiApprover.ApprovePublicApi(assembly);
+            await PublicApiApprover.ApprovePublicApiAsync(assembly);
         }
 
         internal static class PublicApiApprover
         {
-            public static void ApprovePublicApi(Assembly assembly)
+            public static async Task ApprovePublicApiAsync(Assembly assembly)
             {
                 var publicApi = ApiGenerator.GeneratePublicApi(assembly, new ApiGeneratorOptions());
-                var writer = new ApprovalTextWriter(publicApi, "cs");
-                var approvalNamer = new AssemblyPathNamer(assembly.Location);
-                Approvals.Verify(writer, approvalNamer, Approvals.GetReporter());
-            }
-        }
-
-        internal class AssemblyPathNamer : UnitTestFrameworkNamer
-        {
-            private readonly string _name;
-
-            public AssemblyPathNamer(string assemblyPath)
-            {
-                _name = Path.GetFileNameWithoutExtension(assemblyPath);
-
-            }
-            public override string Name
-            {
-                get { return _name; }
+                await Verifier.Verify(publicApi);
             }
         }
     }
